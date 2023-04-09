3c6434f9768d488add54baa94f0cfe261382c55e
Shahid <shahid451998@gmail.com>
2023-04-09 09:15:15 +0530

    list

---
+++ list_example.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    list<int>ls;
+    ls.push_back(5);
+    ls.emplace_back(6);
+    ls.emplace_front(4);
+    ls.push_front(3);
+    //all similar to vector
+    
+}
